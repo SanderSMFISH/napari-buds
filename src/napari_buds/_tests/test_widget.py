67c7e57326a7dd0622c896f9e8276d4eded5d065
SanderSMFISH <102157692+SanderSMFISH@users.noreply.github.com>
2022-08-17 11:37:13 +0200

    Update test_widget.py

--- src/napari_buds/_tests/test_widget.py
+++ src/napari_buds/_tests/test_widget.py
@@ -9,7 +9,3 @@ def test_Main(make_napari_viewer, capsys):
 
     # create our widget, passing in the viewer
     my_widget = Main(viewer)
-
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == f"you have selected {layer}\n"
