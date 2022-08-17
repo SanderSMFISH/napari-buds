8b901b0bbb6b8b94fcc4065c68728f46d405e53d
SanderSMFISH <102157692+SanderSMFISH@users.noreply.github.com>
2022-08-17 11:24:22 +0200

    Update test_widget.py

--- src/napari_buds/_tests/test_widget.py
+++ src/napari_buds/_tests/test_widget.py
@@ -5,7 +5,7 @@ from .._widget import Main
 def test_Main(make_napari_viewer, capsys):
     viewer = make_napari_viewer()
     layer = viewer.add_image(np.random.random((100, 100)))
-    layer = viewer.add_labels(np.random.random((100, 100)))
+    layer = viewer.add_labels(np.random.random((100, 100),dtype=int))
 
     # create our widget, passing in the viewer
     my_widget = Main(viewer)
