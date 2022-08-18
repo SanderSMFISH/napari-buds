9b2e042b731e262ca79fe3ba8f8e36aeaf17fe33
Sander van Otterdijk <scvanotterdijk@gmail.com>
2022-08-18 16:59:41 +0200

    Update test_widget.py

--- src/napari_buds/_tests/test_widget.py
+++ src/napari_buds/_tests/test_widget.py
@@ -1,17 +1,18 @@
 import numpy as np
+from skimage.data import astronaut
 
 from .._widget import Main
 
 def test_Main(make_napari_viewer, capsys):
     viewer = make_napari_viewer()
-    img = viewer.add_image(np.random.random((100, 100)))
-    labels=np.zeros((100, 100),dtype=int)
+    img = viewer.add_image(astronaut()[:,:,0],name='Image')
 
+    labels=np.zeros(img.data.shape,dtype=int)
     label=1
     for i in range(0,100,34):
-        labels[i,:]=label
+        labels[:,i]=label
         label+=1
-    Labels= viewer.add_labels(labels, name='Labels')
+    Labels= viewer.add_labels(labels,name='Labels')
 
     # create our widget, passing in the viewer
     my_widget = Main(viewer)
