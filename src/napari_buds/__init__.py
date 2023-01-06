d6b68961cefc511a1db8f7f39250ec329bf19a3d
Sander van Otterdijk <scvanotterdijk@gmail.com>
2023-01-06 16:40:26 +0100

    updated

--- src/napari_buds/__init__.py
+++ src/napari_buds/__init__.py
@@ -2,7 +2,6 @@ __version__ = "0.1.3"
 
 from ._reader import napari_get_reader
 from ._sample_data import make_sample_data
-#from ._widget import Main
 from ._widget_overview import UIWidget
 from ._writer import write_multiple, write_single_image
 from napari import viewer
